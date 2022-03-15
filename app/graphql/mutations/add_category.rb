c300f068bf40bba74e5c7b507a6acb7d116f5c0c
Marco <gdmarav374@gmail.com>
2022-03-15 16:13:29 -0500

    Add category mutation

---
+++ app/graphql/mutations/add_category.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Mutations
+  class AddCategory < Mutations::BaseMutation
+    argument :params, Types::Input::CategoryInputType, required: true
+
+    field :category, Types::CategoryType, null: false
+
+    def resolve(params:)
+      category_params = Hash params
+
+      begin
+        category = Category.create!(category_params)
+
+        { category: }
+      rescue ActiveRecord::RecordInvalid => e
+        GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+          " #{e.record.errors.full_messages.join(', ')}")
+      end
+    end
+  end
+end
